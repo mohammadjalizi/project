ef5a5da489efec6d42fffdb0b4a20e155d8a183c
mohammadjalizi <mjalizi1376@gmail.com>
2022-11-26 13:38:55 +0330

    addproject

--- java.js
+++ java.js
@@ -1,11 +1,12 @@
 const list=document.getElementById('list')
 const menu=document.getElementById('menu')
 console.log(menu,list)
-alert()
+
 
 menu.addEventListener("click",()=>{
 
-list.classList.toggle('active')
+list.style.
+transform= "translateX(0)"
 
 
 })
